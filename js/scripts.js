a2b2462b917dbea7b9e89790cc2336384669e461
Ianodad <ianodad@gmail.com>
2018-03-26 17:41:03 +0300

    Added navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
 
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-});
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-});
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
     });
 
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    });
 });
